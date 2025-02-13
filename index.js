69d8ffdb4e82f880c5b7f299ec8ac4d2ee0e6d6f
TobiasAndren <tobiasandren@hotmail.com>
2025-02-13 10:33:41 +0100

    added a base to the index.html file with some styling and color changers

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "src")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+
+app.listen(port, () =>
+  console.log(`server is listening on http://localhost:${port}`)
+);
